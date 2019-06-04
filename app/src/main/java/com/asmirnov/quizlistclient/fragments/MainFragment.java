13a809914fa77eef435941a55924aeaa0f98df42
Smirnov Andrey <smirnof.andrey@gmail.com>
2019-06-04 23:16:30 +0300

    Main page refactoring

--- app/src/main/java/com/asmirnov/quizlistclient/fragments/MainFragment.java
+++ app/src/main/java/com/asmirnov/quizlistclient/fragments/MainFragment.java
@@ -37,12 +37,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainFragment extends Fragment implements View.OnClickListener{
+public class MainFragment extends Fragment{
 
     private static final String TAG = "quizlistLogs";
 
     private MyHttpService myHttpService;
-    private Module currentModule;
 
     private ArrayList<Module> modulesList;
     private ModuleListAdapter adapter;
@@ -50,52 +49,18 @@ public class MainFragment extends Fragment implements View.OnClickListener{
 
     private ListView listViewModules;
 
-    private TextView textInfo;
-
-    private EditText textId;
-    private EditText moduleName;
-    private EditText moduleInfo;
-
-    private Button buttonGetModules;
-    private Button buttonGetModuleById;
-    private Button buttonDeleteModule;
-    private Button buttonCreateModule;
-    private Button buttonUpdateModule;
-    private Button createCard;
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home,container,false);
 
-        textInfo = (TextView) v.findViewById(R.id.textInfo);
-
-        textId = (EditText) v.findViewById(R.id.editText);
-        moduleName = (EditText) v.findViewById(R.id.editModuleName);
-        moduleInfo = (EditText) v.findViewById(R.id.editModuleInfo);
-
-        buttonGetModules = (Button) v.findViewById(R.id.buttonGetModules);
-        buttonGetModuleById = (Button) v.findViewById(R.id.buttonGetModuleById);
-        buttonDeleteModule = (Button) v.findViewById(R.id.buttonDeleteModule);
-        buttonCreateModule = (Button) v.findViewById(R.id.buttonCreateModule);
-        buttonUpdateModule = (Button) v.findViewById(R.id.buttonUpdateModule);
-        createCard = (Button) v.findViewById(R.id.createCard);
-
         listViewModules = (ListView) v.findViewById(R.id.listView);
         listViewModules.addHeaderView(createHeader("Modules"));
 
-        buttonGetModules.setOnClickListener(this);
-        buttonGetModuleById.setOnClickListener(this);
-        buttonDeleteModule.setOnClickListener(this);
-        buttonCreateModule.setOnClickListener(this);
-        buttonUpdateModule.setOnClickListener(this);
-        createCard.setOnClickListener(this);
-
         // module list
         modulesList = new ArrayList<>();
-        currentModule = new Module(12,"no modules","no modules info");
-        modulesList.add(currentModule);
+        modulesList.add(new Module(0,"no modules",""));
         adapter = new ModuleListAdapter(getActivity(), modulesList);
         listViewModules.setAdapter(adapter);
 
@@ -104,14 +69,14 @@ public class MainFragment extends Fragment implements View.OnClickListener{
             public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
                                     long id) {
                 try{
-                    currentModule = modulesList.get((int)id);
+                    Module currentModule = modulesList.get((int)id);
+                    Intent intent = new Intent(getActivity(), ActivityCards.class);
+                    intent.putExtra("currentModule", currentModule);
+                    intent.putExtra("myHttpService", myHttpService);
+                    startActivity(intent);
                 }catch (Exception e){
-
+                    Log.d(TAG, "onItemClick: error in getting module form list. id="+id);
                 }
-                Intent intent = new Intent(getActivity(), ActivityCards.class);
-                intent.putExtra("currentModule", currentModule);
-                intent.putExtra("myHttpService", myHttpService);
-                startActivity(intent);
             }
         });
 
@@ -127,32 +92,6 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     public void onResume() {
         super.onResume();
         getUserModules();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.buttonGetModules:
-                getUserModules();
-                break;
-            case R.id.buttonGetModuleById:
-                getModuleById();
-                break;
-            case R.id.buttonUpdateModule:
-                updateModule();
-                break;
-            case R.id.buttonCreateModule:
-                createModule();
-                break;
-            case R.id.buttonDeleteModule:
-                deleteModule();
-                break;
-            case R.id.createCard:
-                createCard();
-                break;
-            default:
-                break;
-        }
     }
 
     private void refreshMyHttpService() {
@@ -203,30 +142,15 @@ public class MainFragment extends Fragment implements View.OnClickListener{
 
                     adapter.notifyDataSetChanged();
 
-                    textInfo.setText("Congrats, Success!");
-
-//                            String json = response.body().toString();
-//                            Gson gson = new GsonBuilder().create();
-//
-//                            Type type = new TypeToken<List<Module>>(){}.getType();
-//                            List<Module> modules = gson.fromJson(json, type);
-//
-//                            ///
-//
-//                            Gson gson = new Gson();
-//                            String data = gson.toJson(response.body)
-//                            ToolsItem toolsItem = gson.fromJson(data,ToolsItem.class);
-//                            toolItem.mConstructor(response.body());
-
                 }else{
                     int rawCode = response.raw().code();
                     switch (rawCode){
                         case 500:{
                             Log.d(TAG, "not valid token. need to refresh it!");
+                            Toast.makeText(getActivity().getApplicationContext(), "not valid token. need to refresh it!", Toast.LENGTH_LONG).show();
                             break;
                         }
                     }
-                    textInfo.setText("not success, server response code:"+rawCode);
                     Log.d(TAG, "getting user modules from srv. error. server response code:"+rawCode);
                 }
             }
@@ -234,168 +158,11 @@ public class MainFragment extends Fragment implements View.OnClickListener{
             @Override
             public void onFailure(Call<List<Module>> call, Throwable t) {
                 t.printStackTrace();
-                textInfo.setText(t.getMessage());
                 Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
                 Log.d(TAG,"getting user modules from srv. error:"+t.getMessage());
+                refreshMyListByTestValues();
             }
         });
-    }
-
-    private void createCard() {
-        refreshMyHttpService();
-
-        Module choosenModule=null;
-        Integer currId = Integer.parseInt(textId.getText().toString());
-        for (Module module : modules) {
-            if(module.getId() == currId){
-                choosenModule = module;
-            }
-        }
-        if(choosenModule == null){
-            return;
-        }
-
-        Call<Card> call = myHttpService.getServerQuery().createCard(textId.getText().toString(),
-                new Card(choosenModule,moduleName.getText().toString(),moduleInfo.getText().toString()));
-
-        call.enqueue(new Callback<Card>() {
-            @Override
-            public void onResponse(Call<Card> call, Response<Card> response) {
-                if (response.isSuccessful()) {
-                    Card card = response.body();
-                    Log.d(TAG,"create new card:"+card.toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Card> call, Throwable t) {
-                t.printStackTrace();
-                Log.d(TAG,"create new card. error:"+t.getMessage());
-            }
-        });
-    }
-
-    private void getModuleById() {
-        refreshMyHttpService();
-
-        Call<Module> call = myHttpService.getServerQuery().getModuleById(textId.getText().toString());
-
-        call.enqueue(new Callback<Module>() {
-            @Override
-            public void onResponse(Call<Module> call, Response<Module> response) {
-                if (response.isSuccessful()) {
-
-                    modules.clear();
-                    try {
-                        modules.add(response.body());
-
-                        modulesList.clear();
-                        modulesList.addAll((ArrayList<Module>) modules);
-
-                        adapter.notifyDataSetChanged();
-
-                        textInfo.setText("Congrats, Success!");
-                    }catch(Exception e){
-                        textInfo.setText("Exception in getting module");
-                    }
-                }else{
-                    int rawCode = response.raw().code();
-                    switch (rawCode){
-                        case 500:{
-                            // we have to refresh token
-                            break;
-                        }
-                    }
-                    textInfo.setText("not success, server response code:"+rawCode);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Module> call, Throwable t) {
-                t.printStackTrace();
-                textInfo.setText(t.getMessage());
-                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void createModule() {
-        refreshMyHttpService();
-
-        currentModule = new Module(moduleName.getText().toString(),
-                moduleInfo.getText().toString());
-
-        Call<Module> call = myHttpService.getServerQuery().createModule(currentModule);
-
-        call.enqueue(new Callback<Module>() {
-            @Override
-            public void onResponse(Call<Module> call, Response<Module> response) {
-                if (response.isSuccessful()) {
-                    Module module = response.body();
-                    if(module == null){
-
-                    }else{
-                        currentModule = module;
-                        getUserModules();
-                    }
-                }
-                textInfo.setText("response code:"+response.code());
-            }
-
-            @Override
-            public void onFailure(Call<Module> call, Throwable t) {
-                t.printStackTrace();
-                textInfo.setText(t.getMessage());
-                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void updateModule() {
-        refreshMyHttpService();
-
-        Call<Module> call = myHttpService.getServerQuery().updateModule(textId.getText().toString(),
-                new Module(moduleName.getText().toString(),
-                        moduleInfo.getText().toString()));
-
-        call.enqueue(new Callback<Module>() {
-            @Override
-            public void onResponse(Call<Module> call, Response<Module> response) {
-                //if (response.isSuccessful()) {
-                textInfo.setText("response code:"+response.code());
-                //}
-            }
-
-            @Override
-            public void onFailure(Call<Module> call, Throwable t) {
-                t.printStackTrace();
-                textInfo.setText(t.getMessage());
-                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void deleteModule() {
-        refreshMyHttpService();
-
-        Call<Integer> call = myHttpService.getServerQuery().deleteModule(textId.getText().toString());
-
-        call.enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(Call<Integer> call, Response<Integer> response) {
-                //if (response.isSuccessful()) {
-                textInfo.setText("Delete successful:"+response.code());
-                //}
-            }
-
-            @Override
-            public void onFailure(Call<Integer> call, Throwable t) {
-                t.printStackTrace();
-                textInfo.setText(t.getMessage());
-                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-
     }
 
 }
